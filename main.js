ec48fd17aeea1115790eea4cb415a9b91494ba9a
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-05-02 00:40:53 +0100

    WIP: learning Vue.js off VueMastery

---
+++ main.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    description: 'Most amazing pair ever designed!',
+    image: './assets/vue-socks-green.jpg',
+    url: 'https://google.com',
+    inStock: true,
+    inventory: 8,
+    onSale: true,
+    details: ['60% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vue-socks-green.jpg',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vue-socks-blue.jpg',
+      },
+    ],
+    cart: 0,
+    sizes: ['small', 'medium', 'large'],
+  },
+  methods: {
+    addToCart: function () {
+      this.cart += 1;
+    },
+    /**
+     * Anonymous functions can also be written as below but not all browsers supports it
+     * @param {} variantImage
+     */
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    reduceCart() {
+      this.cart -= 1;
+    },
+  },
+});
