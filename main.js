1b7bd3a57fab02225a344cd5af277632348aa968
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-05-17 12:13:38 +0100

    Feat: adding a form component

--- main.js
+++ main.js
@@ -49,6 +49,18 @@ Vue.component('product', {
       </button>
     </div>
   <br /> <br />
+    <div>
+      <h2>Reviews</h2>
+      <p v-if="!reviews.length"> There are no reviews yet.</p>
+      <ul>
+        <li v-for="review in reviews">
+        <p> {{ review.name }} </p>
+        <p>Rating: {{ review.rating }} </p>
+        <p> {{ review.review }} </p>
+        </li>
+      </ul>
+    </div>
+
     <product-review @review-submitted="addReview"></product-review>
 
   </div>
@@ -145,6 +157,14 @@ Vue.component('product', {
 Vue.component('product-review', {
   template: `
       <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors"> {{ error }} </li>
+        </ul>
+      </p>
+
       <p>
         <label for="name">Name:</label>
         <input id="name" v-model="name" placeholder="name">
@@ -179,19 +199,32 @@ Vue.component('product-review', {
       name: null,
       review: null,
       rating: null,
+      errors: [],
     };
   },
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating,
-      };
-      this.$emit('review-submitted', productReview);
-      this.name = null;
-      this.review = null;
-      this.rating = null;
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        };
+        this.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) {
+          this.errors.push('Name required');
+        }
+        if (!this.review) {
+          this.errors.push('Review required');
+        }
+        if (!this.rating) {
+          this.errors.push('Rating required');
+        }
+      }
     },
   },
 });
